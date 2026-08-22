0f6fccf7289cedc93f131d87718960e125858bec
Tyler Gushue <Tyler.Gushue@dal.ca>
2026-08-22 13:47:42 -0300

    Created the GUI file.  Create basic layout with a label, ability to select file, button for starting declutter, and a message box for reports while decluttering.  Also added a logo to application.

---
+++ src/gui.py
@@ -0,0 +1,119 @@
+import os
+from tkinter import *
+from tkinter import filedialog, messagebox, scrolledtext
+from tkinter.ttk import Progressbar
+
+# --- Color Palette Constants ---
+BG = "#fbfcfc"
+PRIMARY = "#59b09d"
+SECONDARY = "#97d9ca"
+ACCENT = "#71d8c1"
+TEXT= "#070908"  
+
+# Creates default window dimensions and titles it
+
+window = Tk()
+window.title("Desktop Declutter")
+window.geometry("500x750")
+window.config(padx=20, pady=20, bg=BG)
+window.resizable(False, False)
+
+# Sets logo
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+logo_path = os.path.join(script_dir, "Logo.png")
+
+try:
+    app_icon = PhotoImage(file=logo_path)
+    window.iconphoto(True, app_icon)
+except Exception as e:
+    print(f"Could not load icon: {e}")
+
+# Creates label of application
+lbl = Label(
+     window, 
+     text="Desktop Declutter", 
+     font=("Arial Bold", 20),
+     bg=BG, 
+    fg=TEXT
+)
+lbl.pack(pady=20)
+
+
+# Section for selecting a folder to declutter
+
+folder_selected = "No folder was selected"
+
+def choose_folder():
+        folder_selected = filedialog.askdirectory(title="Select a Folder to Declutter")
+        if folder_selected:
+            path_label.config(text=folder_selected)
+        else:
+            folder_selected = "No folder was selected"
+
+row_frame = Frame(window)
+row_frame.pack(pady=20, fill="x")
+
+btn = Button(
+     row_frame, 
+     text="Select Folder", 
+     command=choose_folder,
+     bg=ACCENT,
+     fg="white",
+     activebackground=PRIMARY,
+     relief="flat",
+     padx=12,
+     pady=6,
+     cursor="hand2"
+)
+btn.pack(side=LEFT, padx=(0, 15))
+
+path_label = Label(
+     row_frame, 
+     text="No folder selected", 
+     wraplength=350
+)
+path_label.pack(side=LEFT)
+
+# Start button section
+
+start_btn = Button(
+     window, 
+     text="Start Declutter",
+     font=("Arial Bold", 12),
+     bg=PRIMARY,
+     fg="white",
+     activebackground=PRIMARY,
+     relief="flat",
+     padx=20,
+     pady=10,
+     cursor="hand2"
+)
+start_btn.pack(fill="x", pady=20)
+
+# message box section
+
+msg_box = scrolledtext.ScrolledText(
+     window,
+     width=50,
+     height=8,
+     font=("Segoe UI", 9),
+     bg="#f0f4f3",
+     fg=TEXT,
+     relief="solid",
+     bd=1,
+     state="disabled"
+)
+msg_box.pack(fill="both", expand=True, pady=20)
+
+def log_message(message: str):
+    msg_box.config(state="normal")      
+    msg_box.insert(END, message + "\n") 
+    msg_box.see(END)                    
+    msg_box.config(state="disabled")
+
+# Set initial message and lock the box
+log_message("Select a folder to get started")
+
+# Keeps window opened
+window.mainloop()
